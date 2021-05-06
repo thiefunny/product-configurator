e15e889a3c94e4b33b37c551814f4373415c3daf
thiefunny <mikolaj@walanus.com>
2021-05-06 20:48:54 +0200

    working version - before fine-tuning

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-// import {
-//     tempEl
-// } from './dom-cache.js'
 import {
     data,
     moduleTypes
@@ -9,12 +6,16 @@ import {
 
 const productConfigurator = document.querySelector('.product__configurator');
 
-const checkItemPicked = _ => {
-
+const checkItemSelected = moduleIndex => {
+    let selectedItem;
+    selectedItem = data[moduleIndex].items.find(item => item.itemSelected === true)
+    return selectedItem.itemName
 }
 
-const pickedItemThumbnail = _ => {
-    return data[0].items[0].itemThumbnailURL
+const selectedItemThumbnail = moduleIndex => {
+    let selectedItem;
+    selectedItem = data[moduleIndex].items.find(item => item.itemSelected === true)
+    return selectedItem.itemThumbnailURL
 }
 
 const generateModuleMarkup = moduleIndex => {
@@ -40,27 +41,22 @@ const generateModuleMarkup = moduleIndex => {
         return filterMarkup;
     }
 
-
-
     const generateItemsGrid = _ => {
 
         let gridMarkup = '';
 
         data[moduleIndex].items.forEach(item => {
-
             gridMarkup += `
-            <li class="item">
-                <div class="item__image"><img src="${item.itemThumbnailURL}" alt="" class="thumbnail-img">
-                            <div class="item__image__loupe"><img src="img/sample_img.png" alt="" class="loupe-img"></div>
+            <li class="item ${item.itemType}">
+                <div class="item__image item__child">
+                    <img src="${item.itemThumbnailURL}" alt="" class="thumbnail__img">
+                <img src="img/sample_img.png" alt="" class="item__image__loupe">
                 </div>
-                <div class="item__title">${item.itemName}</div>
+                <div class="item__title item__child">${item.itemName}</div>
             </li>`
 
         })
-
         return gridMarkup;
-
-
     }
 
     if (data[moduleIndex].moduleType === moduleTypes[2]) {
@@ -71,11 +67,12 @@ const generateModuleMarkup = moduleIndex => {
             
                 <div class="module__title type__three">
                     <div class="module__number">${moduleNumber}.</div> ${data[moduleIndex].title}
+                <div class="item__name">${checkItemSelected(moduleIndex)}</div>
+
                 </div>
-                <div class="item__name">${checkItemPicked()}</div>
             
             <!-- <div class="module__type__three__nav__right"> -->
-                <div class="thumbnail"><img src="${pickedItemThumbnail()}" alt="" class="thumbnail-img"></div>
+                <div class="thumbnail"><img src="${selectedItemThumbnail(moduleIndex)}" alt="" class="thumbnail__img"></div>
                 <div class="module__step__type">${data[moduleIndex].moduleType}</div>
             <!-- </div> -->
         </div>
@@ -96,6 +93,7 @@ const generateModuleMarkup = moduleIndex => {
         </div>
         <div class="module__filter module__type__two__filter">
         <ul class="filter__list">
+        <li class="filter__item">Wszystkie</li>
             ${generateFilter()}
             </ul>
         </div>
@@ -141,13 +139,14 @@ generateHTML();
 
 productConfigurator.addEventListener('click', event => {
 
-// Module's visibility
+    // console.log(event.target)
 
-    let modulesArr = [];
+    const modulesArr = [...document.querySelectorAll('.module__box')];
+    const allItems = [...document.getElementsByClassName(`item`)]
+
+    // Module's visibility
 
     if (event.target.classList.contains("module__title") || event.target.classList.contains("module__step__type")) {
-
-        modulesArr = [...event.target.parentNode.parentNode.parentNode.children];
 
         const moduleIndex = modulesArr.indexOf(event.target.parentNode.parentNode)
 
@@ -155,9 +154,7 @@ productConfigurator.addEventListener('click', event => {
 
             if (elem.edited === true && elem.moduleType !== moduleTypes[0]) {
                 elem.moduleType = moduleTypes[2]
-            }
-
-            else (elem.moduleType = moduleTypes[0])
+            } else(elem.moduleType = moduleTypes[0])
         }
 
         if (data[moduleIndex].moduleType === moduleTypes[0]) {
@@ -170,8 +167,65 @@ productConfigurator.addEventListener('click', event => {
 
         generateHTML();
     }
-})
 
+    // Filter by CSS display
+
+    if (event.target.classList.contains("filter__item")) {
+        const filterItems = [...event.target.parentNode.children];
+
+        const typeFiltered = event.target.innerHTML;
+        const filteredItems = [...document.getElementsByClassName(`item ${typeFiltered}`)]
+
+        allItems.forEach(item => item.classList.add("hidden"));
+        filteredItems.forEach(item => item.classList.remove("hidden"));
+
+        if (typeFiltered === 'Wszystkie') {
+            allItems.forEach(item => item.classList.remove("hidden"));
+
+        }
+    }
+
+    // Select item
+
+    if (event.target.classList.contains("item__child")) {
+        const selectedItemIndex = allItems.indexOf(event.target.parentNode);
+        const moduleIndex = modulesArr.indexOf(event.target.parentNode.parentNode.parentNode.parentNode);
+        const selectedItemEl = event.target.parentNode;
+
+        for (let elem of data[moduleIndex].items) {
+            elem.itemSelected = false;
+        }
+        data[moduleIndex].items[selectedItemIndex].itemSelected = true;
+        data[moduleIndex].edited = true;
+        data[moduleIndex].moduleType = moduleTypes[2];
+        generateHTML();
+
+    } else if (event.target.classList.contains("thumbnail__img")) {
+        const selectedItemIndex = allItems.indexOf(event.target.parentNode.parentNode);
+        const moduleIndex = modulesArr.indexOf(event.target.parentNode.parentNode.parentNode.parentNode.parentNode);
+
+        for (let elem of data[moduleIndex].items) {
+            elem.itemSelected = false;
+        }
+        data[moduleIndex].items[selectedItemIndex].itemSelected = true;
+        data[moduleIndex].edited = true;
+        data[moduleIndex].moduleType = moduleTypes[2];
+        generateHTML();
+
+    }
+
+    // Enlarge image
+
+    if (event.target.classList.contains("item__image__loupe")) {
+        const bigImageContainerEl = document.querySelector(".big__image__container")
+        const closeGalleryEl = document.querySelector(".close__gallery")
+        bigImageContainerEl.classList.remove("hidden");
+        
+        closeGalleryEl.onclick = function() {bigImageContainerEl.classList.add("hidden");}
+
+    }
+
+})
 
 const moduleTitleEls = [...document.querySelectorAll(".module__title")];
 const moduleStepTypeEl = document.querySelector(".module__step__type");
--- js/modules-data.js
+++ js/modules-data.js
@@ -6,6 +6,68 @@ export const itemTypes = ['zielone', 'niebieskie', 'żółte'];
 
 export const data = [{
         title: 'Moduł 1',
+        moduleType: moduleTypes[1],
+        edited: false,
+        items: [{
+                itemName: 'Item 1',
+                itemType: itemTypes[1],
+                itemThumbnailURL: 'img/sample_img.png',
+                itemImageURL: '',
+                itemSelected: false
+            },
+            {
+                itemName: 'Item 2',
+                itemType: itemTypes[0],
+                itemThumbnailURL: 'img/sample_img.png',
+                itemImageURL: '',
+                itemSelected: true
+            },
+            {
+                itemName: 'Item 3',
+                itemType: itemTypes[2],
+                itemThumbnailURL: 'img/sample_img.png',
+                itemImageURL: '',
+                itemSelected: false
+            },
+            {
+                itemName: 'Item 4',
+                itemType: itemTypes[2],
+                itemThumbnailURL: 'img/sample_img.png',
+                itemImageURL: '',
+                itemSelected: false
+            },
+            {
+                itemName: 'Item 5',
+                itemType: itemTypes[2],
+                itemThumbnailURL: 'img/sample_img.png',
+                itemImageURL: '',
+                itemSelected: false
+            },
+            {
+                itemName: 'Item 6',
+                itemType: itemTypes[0],
+                itemThumbnailURL: 'img/sample_img.png',
+                itemImageURL: '',
+                itemSelected: false
+            },
+            {
+                itemName: 'Item 7',
+                itemType: itemTypes[2],
+                itemThumbnailURL: 'img/sample_img.png',
+                itemImageURL: '',
+                itemSelected: false
+            },
+            {
+                itemName: 'Item 8',
+                itemType: itemTypes[1],
+                itemThumbnailURL: 'img/sample_img.png',
+                itemImageURL: '',
+                itemSelected: false
+            },
+        ],
+    },
+    {
+        title: 'Moduł 2',
         moduleType: moduleTypes[0],
         edited: false,
         items: [{
@@ -13,97 +75,35 @@ export const data = [{
                 itemType: itemTypes[1],
                 itemThumbnailURL: 'img/sample_img.png',
                 itemImageURL: '',
-                itemPicked: false
+                itemSelected: false
             },
             {
                 itemName: 'Item 2',
                 itemType: itemTypes[0],
                 itemThumbnailURL: 'img/sample_img.png',
                 itemImageURL: '',
-                itemPicked: false
+                itemSelected: false
             },
             {
                 itemName: 'Item 3',
                 itemType: itemTypes[2],
                 itemThumbnailURL: 'img/sample_img.png',
                 itemImageURL: '',
-                itemPicked: false
+                itemSelected: false
             },
             {
                 itemName: 'Item 4',
                 itemType: itemTypes[2],
                 itemThumbnailURL: 'img/sample_img.png',
                 itemImageURL: '',
-                itemPicked: false
+                itemSelected: false
             },
             {
                 itemName: 'Item 5',
                 itemType: itemTypes[2],
                 itemThumbnailURL: 'img/sample_img.png',
                 itemImageURL: '',
-                itemPicked: false
-            },
-            {
-                itemName: 'Item 6',
-                itemType: itemTypes[0],
-                itemThumbnailURL: 'img/sample_img.png',
-                itemImageURL: '',
-                itemPicked: false
-            },
-            {
-                itemName: 'Item 7',
-                itemType: itemTypes[2],
-                itemThumbnailURL: 'img/sample_img.png',
-                itemImageURL: '',
-                itemPicked: false
-            },
-            {
-                itemName: 'Item 8',
-                itemType: itemTypes[1],
-                itemThumbnailURL: 'img/sample_img.png',
-                itemImageURL: '',
-                itemPicked: false
-            },
-        ],
-    },
-    {
-        title: 'Moduł 2',
-        moduleType: moduleTypes[2],
-        edited: true,
-        items: [{
-                itemName: 'Item 1',
-                itemType: itemTypes[1],
-                itemThumbnailURL: 'img/sample_img.png',
-                itemImageURL: '',
-                itemPicked: false
-            },
-            {
-                itemName: 'Item 2',
-                itemType: itemTypes[0],
-                itemThumbnailURL: 'img/sample_img.png',
-                itemImageURL: '',
-                itemPicked: false
-            },
-            {
-                itemName: 'Item 3',
-                itemType: itemTypes[2],
-                itemThumbnailURL: 'img/sample_img.png',
-                itemImageURL: '',
-                itemPicked: false
-            },
-            {
-                itemName: 'Item 4',
-                itemType: itemTypes[2],
-                itemThumbnailURL: 'img/sample_img.png',
-                itemImageURL: '',
-                itemPicked: false
-            },
-            {
-                itemName: 'Item 5',
-                itemType: itemTypes[2],
-                itemThumbnailURL: 'img/sample_img.png',
-                itemImageURL: '',
-                itemPicked: false
+                itemSelected: false
             },
         ],
     },
@@ -116,35 +116,35 @@ export const data = [{
                 itemType: itemTypes[1],
                 itemThumbnailURL: 'img/sample_img.png',
                 itemImageURL: '',
-                itemPicked: false
+                itemSelected: false
             },
             {
                 itemName: 'Item 2',
                 itemType: itemTypes[0],
                 itemThumbnailURL: 'img/sample_img.png',
                 itemImageURL: '',
-                itemPicked: false
+                itemSelected: false
             },
             {
                 itemName: 'Item 3',
                 itemType: itemTypes[2],
                 itemThumbnailURL: 'img/sample_img.png',
                 itemImageURL: '',
-                itemPicked: false
+                itemSelected: false
             },
             {
                 itemName: 'Item 4',
                 itemType: itemTypes[2],
                 itemThumbnailURL: 'img/sample_img.png',
                 itemImageURL: '',
-                itemPicked: false
+                itemSelected: false
             },
             {
                 itemName: 'Item 5',
                 itemType: itemTypes[2],
                 itemThumbnailURL: 'img/sample_img.png',
                 itemImageURL: '',
-                itemPicked: false
+                itemSelected: false
             },
         ],
     }
